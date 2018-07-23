393120d16f117951c069ad1472662e48483eb769
Emrullah Cengiz <metalhead.emro@hotmail.com>
2018-07-23 18:48:02 +0300

    sec. commit

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <div className="header-container">
+        <div className="header-container scroll-zero">
           <Header />
         </div>
         <div className="content-container">
--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -7,15 +7,28 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-    <div className="logo-container">
-          <img src={require("../../common/images/blog-logo.png")} />
+        <div className="logo-container">
+          <img src={require("../../common/images/blog-logo.png")} draggable="false" />
+          <blockquote>
+            <i>
+              İçinde her an her şey olabilecek,<br />&nbsp;&nbsp; şahsımın kendisine ait blog sitesine hoşgeldin!
+            </i>
+          </blockquote>
         </div>
         <div className="navigation-cont">
           <ul className="nav-list">
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">POSTS</a></li>
-            <li><a href="#">IMAGES</a></li>
-            <li><a href="#">ABOUT</a></li>
+            <li>
+              <a href="#">HOME</a>
+            </li>
+            <li>
+              <a href="#">POSTS</a>
+            </li>
+            <li>
+              <a href="#">IMAGES</a>
+            </li>
+            <li>
+              <a href="#">ABOUT</a>
+            </li>
           </ul>
         </div>
       </div>
